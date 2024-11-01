8fcd5401dde00b8ace5e0aaf050ac59cf5197770
KDW0401 <rlekdns@gmail.com>
2024-11-01 17:39:16 +0900

    no message

--- src/components/admin/AdminChat.jsx
+++ src/components/admin/AdminChat.jsx
@@ -1,18 +1,23 @@
 import AdminSideBar from "./AdminSideBar";
-import '/JCM/css/admin/Chat.css';
+import '../../css/admin/Chat.css';
 
-const AdminChat = () => (
-    <div className="admin-main">
-        <AdminSideBar/>
-        <div className="main-content">
-            <h1>Chat</h1>
-            <hr />
-            <section className="chat">
-                <h2>Chat with Customers</h2>
-                <p>No new messages at the moment.</p>
-            </section>
+
+const AdminChat = () =>{
+    return(
+    <>
+        <div className="chat-main">
+            <AdminSideBar/>
+            <div className="main-content">
+                <h1>Chat</h1>
+                <hr />
+                <section className="chat">
+                    <h2>Chat with Customers</h2>
+                    <p>No new messages at the moment.</p>
+                </section>
+            </div>
         </div>
-    </div>
-);
+    </>
+    )
+}
 
 export default AdminChat;
